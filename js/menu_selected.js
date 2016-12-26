5b422705c1a562a1b86953a2d08f5b9e33686385
cmrusht <P14163204@my365.dmu.ac.uk>
2016-12-26 11:21:01 +0000

    V1.4.1
    Fixed js again for working with all host strings

--- js/menu_selected.js
+++ js/menu_selected.js
@@ -1,12 +1,21 @@
 function menuselect(host){
-	let tab = host.split("/");
-	
-  let tab_stripped = tab[2].split(".");
+	let tab = host.split("frontend-start/");
   
+  let tab_stripped = tab[1].split(".");
   
-  $("#navbar_" + tab_stripped[0]).siblings().removeClass('selected');
-  $("#navbar_" + tab_stripped[0]).addClass('selected');
-  document.getElementById('menu-nav-mobile').value = tab[2];
+  if (tab_stripped[0].length >= 1){
+    
+    $("#navbar_" + tab_stripped[0]).siblings().removeClass('selected');
+    $("#navbar_" + tab_stripped[0]).addClass('selected');
+    document.getElementById('menu-nav-mobile').value = tab[1];
+    
+  }
+  else {
+    $("#navbar_index").siblings().removeClass('selected');
+    $("#navbar_index").addClass('selected');
+    document.getElementById('menu-nav-mobile').value = "index.html";
+  }
+  
 
 	
 }
